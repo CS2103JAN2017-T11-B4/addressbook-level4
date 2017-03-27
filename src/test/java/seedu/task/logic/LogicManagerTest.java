1af904cda0c2726c5f9ba036015d145351d554c7
Evan Yeung <evan.yeung1@gmail.com>
2017-03-27 21:18:31 +0800

    Update code to pass tests

--- src/test/java/seedu/task/logic/LogicManagerTest.java
+++ src/test/java/seedu/task/logic/LogicManagerTest.java
@@ -347,7 +347,7 @@ public class LogicManagerTest {
         Task p3 = helper.generatePersonWithName("key key");
         Task p4 = helper.generatePersonWithName("KEy sduauo");
 
-        List<Task> fourPersons = helper.generatePersonList(p3, p1, p4, p2);
+        List<Task> fourPersons = helper.generatePersonList(p1, p2, p3, p4);
         TaskList expectedAB = helper.generateAddressBook(fourPersons);
         List<Task> expectedList = fourPersons;
         helper.addToModel(model, fourPersons);
@@ -389,7 +389,7 @@ public class LogicManagerTest {
             Tag tag2 = new Tag("longertag2");
             UniqueTagList tags = new UniqueTagList(tag1, tag2);
             Complete complete = new Complete(false);
-            return new Task(name, null, null, tags, complete, new TaskId(System.currentTimeMillis()));
+            return new Task(name, null, null, tags, complete, new TaskId(1));
         }
 
         /**
@@ -406,7 +406,7 @@ public class LogicManagerTest {
                     null,
                     new UniqueTagList(new Tag("tag" + Math.abs(seed)), new Tag("tag" + Math.abs(seed + 1))),
                     new Complete(false),
-                    new TaskId(System.currentTimeMillis())
+                    new TaskId(seed)
             );
         }
 
@@ -493,17 +493,19 @@ public class LogicManagerTest {
             return Arrays.asList(persons);
         }
 
+        private int nextGeneratePersonWithNameId = 0;
         /**
          * Generates a Person object with given name. Other fields will have some dummy values.
          */
         Task generatePersonWithName(String description) throws Exception {
+            nextGeneratePersonWithNameId++;
             return new Task(
                     new Description(description),
                     null,
                     null,
                     new UniqueTagList(new Tag("tag")),
                     new Complete(false),
-                    new TaskId(System.currentTimeMillis())
+                    new TaskId(nextGeneratePersonWithNameId)
             );
         }
     }
